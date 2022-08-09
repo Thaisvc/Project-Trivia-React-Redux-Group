012563929fc3f25ba48fd6d680e7451229f2fc3b
thais <thaisviana081@outlook.com>
2022-08-09 12:46:09 -0300

    add estado passa ate a penultima do 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,61 +1,75 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import { getApiLogin } from '../redux/action/action';
 
 class Game extends Component {
-  componentDidMount() {
-    const { getApi } = this.props;
-    getApi();
+  constructor() {
+    super();
+    this.state = {
+      category: '',
+      correctAanswer: '',
+      incorrectAnswers: [],
+      question: '',
+
+    };
   }
 
-  /* verificaUrlApi = () => {
-  const { stateApi } = this.props;
-  if (stateApi.response_code !== 0) {
-    return <Redirect to="/" />;
+  componentDidMount() {
+    const { stateApi, history } = this.props;
+    console.log(stateApi.results);
+    if (stateApi.results.length > 0) {
+      this.setState({
+        category: stateApi.results[0].category,
+        correctAanswer: stateApi.results[0].correct_answer,
+        incorrectAnswers: stateApi.results[0].incorrect_answers,
+        question: stateApi.results[0].question,
+      });
+    } else {
+      history.push('/');
+    }
   }
-}
- */
+
   render() {
-    const { stateApi } = this.props;
+    // const { stateApi } = this.props;
+    const { category, correctAanswer, incorrectAnswers, question } = this.state;
+    // console.log(category, correctAanswer, incorrectAnswers, question, api);
 
     return (
       <header>
-        {stateApi.response_code === 0 ? ('Carregando...')
-          : console.log(stateApi.response_code) }
-        {stateApi.results.length > 0 ? (
+
+        <div>
+
+          <h1 data-testid="header-player-name">Nome da pessoa</h1>
+          <p data-testid="header-score">Placar: 0</p>
+          <img
+            data-testid="header-profile-picture"
+            src="https://www.gravatar.com/avatar/c19ad9dbaf91c5533605fbf985177ccc"
+            alt="gravatar"
+          />
+
           <div>
-
-            <h1 data-testid="header-player-name">Nome da pessoa</h1>
-            <p data-testid="header-score">Placar: 0</p>
-            <img
-              data-testid="header-profile-picture"
-              src="https://www.gravatar.com/avatar/c19ad9dbaf91c5533605fbf985177ccc"
-              alt="gravatar"
-            />
-
+            <p data-testid="question-category">{category}</p>
+            <p data-testid="question-text">{question}</p>
             <div>
-              <p data-testid="question-category">{stateApi.results[0].category}</p>
-              <p data-testid="question-text">{stateApi.results[0].question}</p>
-              <div>
-                <button type="button" data-testid="correct-answer">
-                  {stateApi.results[0].correct_answer}
+              <button type="button" data-testid="correct-answer">
+                {correctAanswer}
+              </button>
+            </div>
+            {incorrectAnswers.map((questions, index) => (
+              <div key={ questions } data-testid="answer-options">
+
+                <button type="button" data-testid={ `wrong-answer-${index}` }>
+                  {questions}
                 </button>
               </div>
-              {stateApi.results[0].incorrect_answers.map((question, index) => (
-                <div key={ question } data-testid="answer-options">
-
-                  <button type="button" data-testid={ `wrong-answer-${index}` }>
-                    {question}
-                  </button>
-                </div>
-              ))}
-
-            </div>
+            ))}
 
           </div>
-        ) : <Redirect to="/" />}
+
+        </div>
+
       </header>
     );
   }
